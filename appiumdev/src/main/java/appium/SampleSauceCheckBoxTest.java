6780acb48e7c67640616e51114e7fa4f7f2480df
jasonsouryamath <jasonsouryamath@yahoo.com>
2017-03-22 14:43:43 -0700

    Add files via upload

---
+++ appiumdev/src/main/java/appium/SampleSauceCheckBoxTest.java
@@ -0,0 +1,34 @@
+package appium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.android.AndroidDriver;
+import java.net.URL;
+ 
+public class SampleSauceCheckBoxTest {
+ 
+  public static final String USERNAME = "YOUR_USERNAME";
+  public static final String ACCESS_KEY = "YOUR_ACCESS_KEY";
+  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+ 
+  public static void main(String[] args) throws Exception {
+ 
+    DesiredCapabilities capabilities = new DesiredCapabilities();
+    capabilities.setCapability("platformName", "Android");
+    capabilities.setCapability("deviceName", "Samsung Galaxy S4 Emulator");
+    capabilities.setCapability("platformVersion", "4.4");
+    capabilities.setCapability("app", "http://saucelabs.com/example_files/ContactManager.apk");
+    capabilities.setCapability("browserName", "");
+    capabilities.setCapability("deviceOrientation", "portrait");
+    capabilities.setCapability("appiumVersion", "1.5.3");
+ 
+    WebDriver driver = new AndroidDriver<WebElement>(new URL(URL), capabilities);
+ 
+    /**
+     * Test Actions here...
+     */
+ 
+    driver.quit();
+  }
+}
